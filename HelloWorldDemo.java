3e28adfa5ba79881ad9180532b82dd61ff8ff3ca
Swamy <smitha.swamy@nttdata.com>
2018-02-28 16:08:13 +0530

    added java file

---
+++ HelloWorldDemo.java
@@ -0,0 +1,15 @@
+
+public class HelloWorldDemo {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Everyone");
+		System.out.println("Welcome to session!!");
+		
+		System.out.println("first word "+" second word");
+		
+
+	}
+
+}
+
